56d4c8710be3c48e79725285ce6d1ab8969fcb3d
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-05-27 15:23:53 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", description: "A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory", price_per_night: 56, number_of_guests: 4 }
+chouffe = { name: "Chouffe", address: "6 rue ma vie, Tel Aviv E2 7JE", description: "Awesome", price_per_night: 56, number_of_guests: 6 }
+poissonquifretille = { name: "poissonquifretille", address: "7 rue du David Douillet", description: "Perfect", price_per_night: 56, number_of_guests: 9 }
+tournedos = { name: "tournedos", address: "7 rue du David Douillet", description: "Perfect", price_per_night: 56, number_of_guests: 9 }
+
+[ dishoom, chouffe, poissonquifretille, tournedos ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
